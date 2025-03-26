899febe76806c49e59036843e028e598725a78ef
ahmadrazalab <razahmadrsofficial@gmail.com>
2025-03-26 15:24:51 +0530

    aws_eks_addon

---
+++ addons.tf
@@ -0,0 +1,20 @@
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name = aws_eks_cluster.premium_cluster.name
+  addon_name   = "kube-proxy"
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name = aws_eks_cluster.premium_cluster.name
+  addon_name   = "vpc-cni"
+}
+
+resource "aws_eks_addon" "coredns" {
+  cluster_name = aws_eks_cluster.premium_cluster.name
+  addon_name   = "coredns"
+}
+
+
+resource "aws_eks_addon" "eks_pod_identity_agent" {
+  cluster_name = aws_eks_cluster.premium_cluster.name
+  addon_name   = "eks-pod-identity-agent"
+}
