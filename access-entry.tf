144708b8b7027c8eb405077867c37b03157f0483
ahmadrazalab <razahmadrsofficial@gmail.com>
2025-03-26 18:37:14 +0530

    aws_eks_access_entry with dev and admin policy

--- access-entry.tf
+++ access-entry.tf
@@ -5,10 +5,32 @@ resource "aws_eks_access_entry" "admin-user" {
   user_name = "admin-user"
 }
 
-
-resource "aws_eks_access_entry" "administrator" {
-  cluster_name = aws_eks_cluster.premium_cluster.name
+resource "aws_eks_access_policy_association" "admin-user" {
+  cluster_name  = aws_eks_cluster.premium_cluster.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterPolicy"
   principal_arn = aws_iam_user.admin_user.arn
+
+  access_scope {
+    type       = "cluster"
+  }
+}
+
+#########################################33
+
+resource "aws_eks_access_entry" "dev_user" {
+  cluster_name = aws_eks_cluster.premium_cluster.name
+  principal_arn = aws_iam_user.readonly_user.arn
   type = "STANDARD"
-  user_name = "admin"
+  user_name = "dev"
+}
+
+resource "aws_eks_access_policy_association" "dev-user" {
+  cluster_name  = aws_eks_cluster.premium_cluster.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+  principal_arn = aws_iam_user.readonly_user.arn
+
+  access_scope {
+    type       = "namespace"
+    namespaces = ["staging"]
+  }
 }
