ec90e19c5e14743f967823a533a25674b5c146e0
ahmadrazalab <razahmadrsofficial@gmail.com>
2025-03-26 16:35:02 +0530

    addons Version UPgrade

--- addons.tf
+++ addons.tf
@@ -1,20 +1,25 @@
 resource "aws_eks_addon" "kube_proxy" {
   cluster_name = aws_eks_cluster.premium_cluster.name
   addon_name   = "kube-proxy"
+  addon_version = "v1.31.3-eksbuild.2"
+
 }
 
 resource "aws_eks_addon" "vpc_cni" {
   cluster_name = aws_eks_cluster.premium_cluster.name
   addon_name   = "vpc-cni"
+  addon_version = "v1.19.3-eksbuild.1"
 }
 
 resource "aws_eks_addon" "coredns" {
   cluster_name = aws_eks_cluster.premium_cluster.name
   addon_name   = "coredns"
+  addon_version = "v1.11.4-eksbuild.2"
 }
 
 
 resource "aws_eks_addon" "eks_pod_identity_agent" {
   cluster_name = aws_eks_cluster.premium_cluster.name
   addon_name   = "eks-pod-identity-agent"
+  addon_version = "v1.3.5-eksbuild.2"
 }
