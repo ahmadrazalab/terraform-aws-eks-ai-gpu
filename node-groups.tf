e00cc422e0f38044c6e6d61fa068688aea85b4a4
ahmadrazalab <razahmadrsofficial@gmail.com>
2025-03-27 13:49:55 +0530

    feat: enable GPU node group configuration in EKS

--- node-groups.tf
+++ node-groups.tf
@@ -19,27 +19,27 @@ resource "aws_eks_node_group" "application_node_group" {
   }
 }
 
-# resource "aws_eks_node_group" "gpu_node_groups" {
-#   cluster_name    = aws_eks_cluster.premium_cluster.name
-#   node_group_name = "gpu_node_groups"
-#   node_role_arn   = aws_iam_role.node_group_role.arn
-#   subnet_ids      = var.public_subnet_ids
+resource "aws_eks_node_group" "gpu_node_groups" {
+  cluster_name    = aws_eks_cluster.premium_cluster.name
+  node_group_name = "gpu_node_groups"
+  node_role_arn   = aws_iam_role.node_group_role.arn
+  subnet_ids      = var.public_subnet_ids
 
-#   scaling_config {
-#     desired_size = 0
-#     max_size     = 0
-#     min_size     = 0
-#   }
+  scaling_config {
+    desired_size = 0
+    max_size     = 0
+    min_size     = 0
+  }
 
-#   instance_types = var.gpu_instance_types
+  instance_types = var.gpu_instance_types
 
-# update_config {
-#     max_unavailable = var.node_group_max_unavailable
-#   }
-# #   depends_on = [aws_iam_role_policy_attachment.node_group_policies]
-#   ami_type = "AL2_x86_64_GPU"  # Amazon Linux 2 AMI with GPU support
+update_config {
+    max_unavailable = var.node_group_max_unavailable
+  }
+#   depends_on = [aws_iam_role_policy_attachment.node_group_policies]
+  ami_type = "AL2_x86_64_GPU"  # Amazon Linux 2 AMI with GPU support
 
-#   tags = {
-#     Name = "gpu_node_groups"
-#   }
-# }
+  tags = {
+    Name = "gpu_node_groups"
+  }
+}
