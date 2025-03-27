94604c64bba006d13045fbadd7e56247bd6e8b45
ahmadrazalab <razahmadrsofficial@gmail.com>
2025-03-27 13:48:54 +0530

    add IAM roles and policies for EKS managed node groups

--- Managed-Roles.tf
+++ Managed-Roles.tf
@@ -101,3 +101,38 @@ resource "aws_iam_role_policy_attachment" "eks_pod_identity_ebs_csi_driver_polic
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
   role       = aws_iam_role.eks_pod_identity_ebs_csi_driver_role.name
 }
+
+
+
+
+## EKS managed Node Groups ROLE
+
+resource "aws_iam_role" "node_group_role" {
+  name = "EKSNodeGroupRoles"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_read_only" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node_group_role.name
+}
