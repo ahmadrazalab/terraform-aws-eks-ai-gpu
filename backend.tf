1532871b8bf98f3c72f884ceef94f3f5b96d1ca1
ahmadrazalab <razahmadrsofficial@gmail.com>
2025-03-27 13:48:26 +0530

    update .gitignore to include info.sh and add backend.tf for S3 state management

---
+++ backend.tf
@@ -0,0 +1,9 @@
+# terraform {
+#   backend "s3" {
+#     bucket         = "tfstate-eks-bucket"
+#     key            = "production/terraform.tfstate"
+#     region         = "ap-south-1"
+#     encrypt        = true
+#   }
+# }
+
