cf7d7cf4cfcc5d316be2d8d2ce7ac32e821b5014
ahmadrazalab <razahmadrsofficial@gmail.com>
2025-05-04 14:11:17 +0530

    correct ebs_csi_driver addon resource definition in addons.tf

--- addons.tf
+++ addons.tf
@@ -31,9 +31,8 @@ resource "aws_eks_addon" "metrics_server" {
   addon_version = "v0.7.2-eksbuild.3"  
 }
 
-
-resource "aws_eks_addon" "eks_pod_identity_ebs_csi_driver_policy" {
-  cluster_name = aws_eks_cluster.premium_cluster.name
-  addon_name   = "AmazonEBSCSIDriverPolicy"
-  addon_version = "v1.42.0-eksbuild.1" 
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name    = aws_eks_cluster.premium_cluster.name
+  addon_name      = "aws-ebs-csi-driver"
+  addon_version   = "v1.42.0-eksbuild.1"
 }
