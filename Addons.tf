6047e16b20af7d360a60c11a6de53d8ded99dc37
ahmadrazalab <razahmadrsofficial@gmail.com>
2025-05-06 15:07:44 +0530

    Dependency on NODE Groups

--- Addons.tf
+++ Addons.tf
@@ -1,39 +1,48 @@
 resource "aws_eks_addon" "kube_proxy" {
-  cluster_name = aws_eks_cluster.premium_cluster.name
-  addon_name   = "kube-proxy"
-  addon_version = "v1.31.3-eksbuild.2"
+  cluster_name   = aws_eks_cluster.premium_cluster.name
+  addon_name     = "kube-proxy"
+  addon_version  = "v1.31.3-eksbuild.2"
 
+  depends_on = [aws_eks_node_group.application_node_group]
 }
 
 resource "aws_eks_addon" "vpc_cni" {
-  cluster_name = aws_eks_cluster.premium_cluster.name
-  addon_name   = "vpc-cni"
-  addon_version = "v1.19.3-eksbuild.1"
+  cluster_name   = aws_eks_cluster.premium_cluster.name
+  addon_name     = "vpc-cni"
+  addon_version  = "v1.19.3-eksbuild.1"
+
+  depends_on = [aws_eks_node_group.application_node_group]
 }
 
 resource "aws_eks_addon" "coredns" {
-  cluster_name = aws_eks_cluster.premium_cluster.name
-  addon_name   = "coredns"
-  addon_version = "v1.11.4-eksbuild.10"  
+  cluster_name   = aws_eks_cluster.premium_cluster.name
+  addon_name     = "coredns"
+  addon_version  = "v1.11.4-eksbuild.10"
+
+  depends_on = [aws_eks_node_group.application_node_group]
 }
 
-
 resource "aws_eks_addon" "eks_pod_identity_agent" {
-  cluster_name = aws_eks_cluster.premium_cluster.name
-  addon_name   = "eks-pod-identity-agent"
-  addon_version = "v1.3.5-eksbuild.2"
+  cluster_name   = aws_eks_cluster.premium_cluster.name
+  addon_name     = "eks-pod-identity-agent"
+  addon_version  = "v1.3.5-eksbuild.2"
+
+  depends_on = [aws_eks_node_group.application_node_group]
 }
 
 resource "aws_eks_addon" "metrics_server" {
-  cluster_name = aws_eks_cluster.premium_cluster.name
-  addon_name   = "metrics-server"
-  addon_version = "v0.7.2-eksbuild.3"  
+  cluster_name   = aws_eks_cluster.premium_cluster.name
+  addon_name     = "metrics-server"
+  addon_version  = "v0.7.2-eksbuild.3"
+
+  depends_on = [aws_eks_node_group.application_node_group]
 }
 
 resource "aws_eks_addon" "ebs_csi_driver" {
-  cluster_name    = aws_eks_cluster.premium_cluster.name
-  addon_name      = "aws-ebs-csi-driver"
-  addon_version   = "v1.42.0-eksbuild.1"
+  cluster_name   = aws_eks_cluster.premium_cluster.name
+  addon_name     = "aws-ebs-csi-driver"
+  addon_version  = "v1.42.0-eksbuild.1"
   service_account_role_arn = aws_iam_role.eks_pod_identity_ebs_csi_driver_role.arn
-  
+
+  depends_on = [aws_eks_node_group.application_node_group]
 }
