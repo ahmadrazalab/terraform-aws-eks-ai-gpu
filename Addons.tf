b889d65ba8f36838add9d73ff61d8f3e16748ed7
ahmadrazalab <razahmadrsofficial@gmail.com>
2025-05-06 14:55:58 +0530

    add EKS addon configurations for kube-proxy, vpc-cni, coredns, eks-pod-identity-agent, metrics-server, and ebs-csi-driver

--- Addons.tf
+++ Addons.tf
@@ -14,7 +14,7 @@ resource "aws_eks_addon" "vpc_cni" {
 resource "aws_eks_addon" "coredns" {
   cluster_name = aws_eks_cluster.premium_cluster.name
   addon_name   = "coredns"
-  addon_version = "v1.11.4-eksbuild.10"  # v1.11.4-eksbuild.2 # old 
+  addon_version = "v1.11.4-eksbuild.10"  
 }
 
 
