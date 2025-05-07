fe9863d99830392689ae0e9c45813a1e0bdd4e75
ahmadrazalab <razahmadrsofficial@gmail.com>
2025-05-07 15:31:00 +0530

    feat: refactor IAM roles and access entries for EKS admin group

--- AccessEntry.tf
+++ AccessEntry.tf
@@ -1,5 +1,19 @@
-resource "aws_iam_role" "eks_dev_role" {
-  name = "EKSDevRole"
+resource "aws_iam_group" "eks_admins" {
+  name = "eks-admins"
+}
+
+resource "aws_iam_user" "ahmad" {
+  name = "ahmad"
+}
+
+resource "aws_iam_user_group_membership" "ahmad_group_membership" {
+  user   = aws_iam_user.ahmad.name
+  groups = [aws_iam_group.eks_admins.name]
+}
+
+
+resource "aws_iam_role" "eks_admin_role" {
+  name = "EKSAdminRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -8,8 +22,7 @@ resource "aws_iam_role" "eks_dev_role" {
         Effect = "Allow",
         Principal = {
           AWS = [
-            aws_iam_user.dev1.arn,
-            aws_iam_user.dev2.arn
+            for user in aws_iam_group.eks_admins.users : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${user}"
           ]
         },
         Action = "sts:AssumeRole"
@@ -18,51 +31,24 @@ resource "aws_iam_role" "eks_dev_role" {
   })
 }
 
-
-resource "aws_iam_policy" "assume_dev_role" {
-  name = "AllowAssumeEKSDevRole"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = "sts:AssumeRole",
-        Resource = aws_iam_role.eks_dev_role.arn
-      }
-    ]
-  })
+resource "aws_iam_role_policy_attachment" "eks_admin_policy_attach" {
+  role       = aws_iam_role.eks_admin_role.name
+  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 }
 
-resource "aws_iam_group_policy_attachment" "dev_assume_attach" {
-  group      = aws_iam_group.eks_devs.name
-  policy_arn = aws_iam_policy.assume_dev_role.arn
-}
-
-
-
-data "aws_caller_identity" "current" {}
-
-data "aws_iam_role" "sso_eks_dev" {
-  name = "AWSReservedSSO_EKSDevAccess_1234567890abcdef"  # Replace with actual role name
-}
-
-resource "aws_eks_access_entry" "dev_entry" {
+resource "aws_eks_access_entry" "eks_admin_entry" {
   cluster_name  = aws_eks_cluster.premium_cluster.name
-  principal_arn = data.aws_iam_role.sso_eks_dev.arn
-  user_name     = "eks-devs"
+  principal_arn = aws_iam_role.eks_admin_role.arn
+  user_name     = "eks-admins"
+  type          = "STANDARD"
 }
 
-
-
-
-resource "aws_eks_access_policy_association" "dev_policy" {
+resource "aws_eks_access_policy_association" "eks_admin_association" {
   cluster_name  = aws_eks_cluster.premium_cluster.name
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
-  principal_arn = aws_iam_role.eks_dev_role.arn
+  principal_arn = aws_iam_role.eks_admin_role.arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   access_scope {
-    type       = "namespace"
-    namespaces = ["staging"]
+    type = "cluster"
   }
 }
