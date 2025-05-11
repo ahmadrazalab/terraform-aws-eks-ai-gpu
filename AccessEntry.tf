fb85cb6589b48979e1be690ddff48ac4a58b9c07
ahmadrazalab <razahmadrsofficial@gmail.com>
2025-05-11 16:52:27 +0530

    feat: enable GPU node group configuration in EKS setup

--- AccessEntry.tf
+++ AccessEntry.tf
@@ -1,54 +1,54 @@
-resource "aws_iam_group" "eks_admins" {
-  name = "eks-admins"
-}
+# resource "aws_iam_group" "eks_admins" {
+#   name = "eks-admins"
+# }
 
-resource "aws_iam_user" "ahmad" {
-  name = "ahmad"
-}
+# resource "aws_iam_user" "ahmad" {
+#   name = "ahmad"
+# }
 
-resource "aws_iam_user_group_membership" "ahmad_group_membership" {
-  user   = aws_iam_user.ahmad.name
-  groups = [aws_iam_group.eks_admins.name]
-}
+# resource "aws_iam_user_group_membership" "ahmad_group_membership" {
+#   user   = aws_iam_user.ahmad.name
+#   groups = [aws_iam_group.eks_admins.name]
+# }
 
 
-resource "aws_iam_role" "eks_admin_role" {
-  name = "EKSAdminRole"
+# resource "aws_iam_role" "eks_admin_role" {
+#   name = "EKSAdminRole"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          AWS = [
-            for user in aws_iam_group.eks_admins.users : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${user}"
-          ]
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Principal = {
+#           AWS = [
+#             for user in aws_iam_group.eks_admins.users : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${user}"
+#           ]
+#         },
+#         Action = "sts:AssumeRole"
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "eks_admin_policy_attach" {
-  role       = aws_iam_role.eks_admin_role.name
-  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-}
+# resource "aws_iam_role_policy_attachment" "eks_admin_policy_attach" {
+#   role       = aws_iam_role.eks_admin_role.name
+#   policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+# }
 
-resource "aws_eks_access_entry" "eks_admin_entry" {
-  cluster_name  = aws_eks_cluster.premium_cluster.name
-  principal_arn = aws_iam_role.eks_admin_role.arn
-  user_name     = "eks-admins"
-  type          = "STANDARD"
-}
+# resource "aws_eks_access_entry" "eks_admin_entry" {
+#   cluster_name  = aws_eks_cluster.premium_cluster.name
+#   principal_arn = aws_iam_role.eks_admin_role.arn
+#   user_name     = "eks-admins"
+#   type          = "STANDARD"
+# }
 
-resource "aws_eks_access_policy_association" "eks_admin_association" {
-  cluster_name  = aws_eks_cluster.premium_cluster.name
-  principal_arn = aws_iam_role.eks_admin_role.arn
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+# resource "aws_eks_access_policy_association" "eks_admin_association" {
+#   cluster_name  = aws_eks_cluster.premium_cluster.name
+#   principal_arn = aws_iam_role.eks_admin_role.arn
+#   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
-  access_scope {
-    type = "cluster"
-  }
-}
+#   access_scope {
+#     type = "cluster"
+#   }
+# }
--- Node-groups.tf
+++ Node-groups.tf
@@ -34,50 +34,54 @@ resource "aws_eks_node_group" "application_node_group" {
 
 
 ######################## GPU NODE GROUP ########################
-# resource "aws_eks_node_group" "gpu_node_groups" {
-#   cluster_name    = aws_eks_cluster.premium_cluster.name
-#   version = var.eks-node-version
-#   node_group_name = "gpu_node_groups-${random_string.name_suffix.result}"
-#   node_role_arn   = aws_iam_role.node_group_role.arn
-#   subnet_ids      = var.public_subnet_ids
+resource "aws_eks_node_group" "gpu_node_groups" {
+  cluster_name    = aws_eks_cluster.premium_cluster.name
+  version = var.eks-node-version
+  node_group_name = "gpu_node_groups-${random_string.name_suffix.result}"
+  node_role_arn   = aws_iam_role.node_group_role.arn
+  subnet_ids      = var.public_subnet_ids
 
-#   scaling_config {
-#     desired_size = var.gpu_node_group_desired_size
-#     max_size     = var.gpu_node_group_max_size
-#     min_size     = var.gpu_node_group_min_size
-#   }
+  # lifecycle {
+  #   create_before_destroy = true
+  # }
 
-#   instance_types = var.gpu_instance_types
-#   disk_size = 40
-#   # addding taints on all the nodes created by this node group
-#   taint {
-#     key = "gpu"
-#     value = "t4"
-#     effect = "NO_SCHEDULE"
-#   }
+  scaling_config {
+    desired_size = var.gpu_node_group_desired_size
+    max_size     = var.gpu_node_group_max_size
+    min_size     = var.gpu_node_group_min_size
+  }
 
-#   # Adding labels to the nodes in this node group
-#   labels = {
-#     gpu = "t4"
-#     node-type = "gpu"
-#   }
+  instance_types = var.gpu_instance_types
+  disk_size = 30
+  # addding taints on all the nodes created by this node group
+  taint {
+    key = "gpu"
+    value = "t4"
+    effect = "NO_SCHEDULE"
+  }
+
+  # Adding labels to the nodes in this node group
+  labels = {
+    gpu = "t4"
+    node-type = "gpu"
+  }
 
 
-# update_config {
-#     max_unavailable = var.gpu_node_group_max_unavailable
-#   }
-#   # depends_on = [aws_iam_role_policy_attachment.node_group_policies]
-#   ami_type = "AL2_x86_64_GPU"  # Amazon Linux 2 AMI with GPU support
+update_config {
+    max_unavailable = var.gpu_node_group_max_unavailable
+  }
+  # depends_on = [aws_iam_role_policy_attachment.node_group_policies]
+  ami_type = "AL2_x86_64_GPU"  # Amazon Linux 2 AMI with GPU support
 
-# node_repair_config {
-#   enabled = true
-# }
+node_repair_config {
+  enabled = true
+}
 
-#   tags = {
-#     Name = "gpu_node_groups"
+  tags = {
+    Name = "gpu_node_groups"
 
-#   }
-# }
+  }
+}
 
 ################ ARM NODE GROUP ######################
 
